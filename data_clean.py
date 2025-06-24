d10c9735783f85e8dfc3fd806cdd7c388b6abf9b
AveryJD <averyjdoiron@icloud.com>
2025-06-23 23:37:07 -0300

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -12,7 +12,7 @@ for season in constants.SEASONS:
     for data_set in ['skaters', 'goalies', 'teams']:
 
         # Load the CSV file
-        file_path = f'raw_data/{season}_{data_set}.csv'
+        file_path = f'data_raw/{season}_{data_set}.csv'
         df = pd.read_csv(file_path)
 
         # Define replacements
@@ -28,3 +28,25 @@ for season in constants.SEASONS:
 
         # Save data
         df.to_csv(file_path, index=False)
+
+
+
+# ====================================================================================================
+# SCRIPT TO CHANGE TEAM NAMES TO THEIR ABREVIATIONS IN STANDINGS FILES
+# ====================================================================================================
+
+
+for season in constants.SEASONS:
+    file_path = f'data_standings/{season}.csv'
+    
+    # Read CSV and rename the 2nd column to 'Team'
+    df = pd.read_csv(file_path, header=0)
+    if df.columns[1] != 'Team':
+        df.columns.values[1] = 'Team'
+
+    # Replace full names with abbreviations
+    df['Team'] = df['Team'].map(constants.TEAM_NAME_MAP).fillna(df['Team'])  
+
+    # Save updated CSV
+    df.to_csv(file_path, index=False)
+
