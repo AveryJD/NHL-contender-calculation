b20e6d1d7b5da95f47492fcc419fe10bc50e562b
AveryJD <averyjdoiron@icloud.com>
2025-06-25 23:54:03 -0300

    Update data_relevant.py

--- data_relevant.py
+++ data_relevant.py
@@ -3,20 +3,15 @@
 import pandas as pd
 import constants
 
-
 # ====================================================================================================
-# FUNCTION FOR GATHERING RELEVANT DATA
+# FUNCTION FOR GATHERING DATA TO USE FOR SCORING TEAMS
 # ====================================================================================================
 
-def get_relevant_data(season: str, team_abbrev: str) -> pd.Series:
-    """
-    ADD
-    """
+def get_scoring_data(season: str, team_abbrev: str) -> pd.Series:
 
-    # Load data
-    skaters_df = pd.read_csv(f'data_raw/{season}_skaters.csv')
-    goalies_df = pd.read_csv(f'data_raw/{season}_goalies.csv')
-    teams_df = pd.read_csv(f'data_raw/{season}_teams.csv')
+    # Load raw data
+    skaters_df = pd.read_csv(f'raw_data/team_data/{season}_skaters.csv')
+    goalies_df = pd.read_csv(f'raw_data/team_data/{season}_goalies.csv')
 
 
     # Get dataframe of forwards of the given team
@@ -32,15 +27,18 @@ def get_relevant_data(season: str, team_abbrev: str) -> pd.Series:
     forward_df['icetime/games_played'] = forward_df['icetime'] / forward_df['games_played']
     forward_df = forward_df.sort_values(by="icetime/games_played", ascending=False).reset_index(drop=True)
 
-    top_six_f = forward_df.iloc[:6]
-    bottom_six_f = forward_df.iloc[6:12]
+    one_three_f = forward_df.iloc[:3]
+    four_six_f = forward_df.iloc[3:6]
+    seven_nine_f = forward_df.iloc[6:9]    
+    ten_twelve_f = forward_df.iloc[9:12]
 
-    # Get forward lines total average game scores
-    top_six_f_score = (top_six_f['gameScore'] / top_six_f['games_played']).sum()
-    bottom_six_f_score = (bottom_six_f['gameScore'] / bottom_six_f['games_played']).sum()
+    # Get the total average game scores of the forward lines
+    one_three_f_score = (one_three_f['gameScore'] / one_three_f['games_played']).sum()
+    four_six_f_score = (four_six_f['gameScore'] / four_six_f['games_played']).sum()
+    seven_nine_f_score = (seven_nine_f['gameScore'] / seven_nine_f['games_played']).sum()
+    ten_twelve_f_score = (ten_twelve_f['gameScore'] / ten_twelve_f['games_played']).sum()
 
     
-
     # Get dataframe of defensemen of the given team
     defense_df = skaters_df[(skaters_df['team'] == team_abbrev) & 
                             (skaters_df['position'] == 'D') &
@@ -50,14 +48,18 @@ def get_relevant_data(season: str, team_abbrev: str) -> pd.Series:
     defense_df = defense_df.sort_values(by="games_played", ascending=False).reset_index(drop=True)
     defense_df = defense_df.iloc[:6]
 
+    # Get different lines of defensemen based on average icetime
     defense_df['icetime/games_played'] = defense_df['icetime'] / defense_df['games_played']
     defense_df = defense_df.sort_values(by="icetime/games_played", ascending=False).reset_index(drop=True)
 
-    top_three_d = defense_df.iloc[:3]
-    bottom_three_d = defense_df.iloc[3:6]
+    one_two_d = defense_df.iloc[:2]
+    three_four_d = defense_df.iloc[2:4]
+    five_six_d = defense_df.iloc[4:6]
 
-    top_three_d_score = (top_three_d['gameScore'] / top_three_d['games_played']).sum()
-    bottom_three_d_score = (bottom_three_d['gameScore'] / bottom_three_d['games_played']).sum()
+    # Get the total average game scores of the defense lines
+    one_two_d_score = (one_two_d['gameScore'] / one_two_d['games_played']).sum()
+    three_four_d_score = (three_four_d['gameScore'] / three_four_d['games_played']).sum()
+    five_six_d_score = (five_six_d['gameScore'] / five_six_d['games_played']).sum()
 
 
     # Get dataframe of goalies of the given team
@@ -68,78 +70,51 @@ def get_relevant_data(season: str, team_abbrev: str) -> pd.Series:
     goalie_df = goalie_df.sort_values(by="games_played", ascending=False).reset_index(drop=True)
     starting_goalie = goalie_df.iloc[0]
 
-    # Get the starting goalie's GSAx
-    starting_goalie_gsax = starting_goalie['xGoals'] - starting_goalie['goals']
-
-
-    # Get the team's even strength data
-    es_df = teams_df[(teams_df['team'] == team_abbrev) & (teams_df['situation'] == '5on5')]
-
-    # Get the team's even strength goals for and against
-    team_gf = es_df.iloc[0]['goalsFor']
-    team_ga = es_df.iloc[0]['goalsAgainst']
-
-    # Get the team's special teams data
-    pp_df = teams_df[(teams_df['team'] == team_abbrev) & (teams_df['situation'] == '5on4')]
-    pk_df = teams_df[(teams_df['team'] == team_abbrev) & (teams_df['situation'] == '4on5')]
-
-    # Get the team's even strength goals for and against
-    team_pp_gf = pp_df.iloc[0]['goalsFor']
-    team_pk_ga = pk_df.iloc[0]['goalsAgainst']
-
-    # Adjust goals for seasons that wern't played with 82 games
-    if season == '2012-2013':
-        season_games = 48
-    elif season == '2019-2020':
-        season_games = 70
-    elif season == '2020-2021':
-        season_games = 56
-    else:
-        season_games = 82
-
-    adjustment = 82 / season_games
-
-    team_gf *= adjustment
-    team_ga *= adjustment
-    team_pp_gf *= adjustment
-    team_pk_ga *= adjustment
+    # Get the starting goalie's GSAx per Game
+    starting_goalie_gsax = (starting_goalie['xGoals'] - starting_goalie['goals']) / starting_goalie['games_played']
 
 
     # Get the given team's playoff results
     result = constants.TEAM_RESULTS.get(season, {}).get(team_abbrev, -1)
+    
 
-
+    # Make the row of data
     data_row = pd.Series({
         'Season': season,
         'Team': team_abbrev,
         'Result': result,
-        'Top 6 F Game Score': top_six_f_score,
-        'Bottom 6 F Game Score': bottom_six_f_score,
-        'Top 3 D Game Score': top_three_d_score,
-        'Bottom 3 D Game Score': bottom_three_d_score,
-        'Starting Goalie GSAx': starting_goalie_gsax,
-        'ES GF': team_gf,
-        'ES GA': team_ga,
-        'PP GF': team_pp_gf,
-        'PK GA': team_pk_ga
+        '1-3 F Avg Game Scores': one_three_f_score,
+        '4-6 F Avg Game Score': four_six_f_score,
+        '7-9 F Avg Game Score': seven_nine_f_score,
+        '9-12 F Avg Game Score': ten_twelve_f_score,
+        '1-2 D Avg Game Score': one_two_d_score,
+        '3-4 D Avg Game Score': three_four_d_score,
+        '5-6 D Avg Game Score': five_six_d_score,
+        'Starting Goalie GSAx/GP': starting_goalie_gsax,
     })
-
 
     return data_row
 
 
 
 # ====================================================================================================
-# SCRIPT TO GET RELEVANT DATA FOR TEAMS OVER MULTIPLE SEASONS
+# SCRIPT TO GET SCORING DATA FOR TEAMS OVER MULTIPLE SEASONS
 # ====================================================================================================
 
-# Initialize relevant data dataframe
-relevant_data = pd.DataFrame(columns=['Season', 'Team', 'Result',
-                                      'Top 6 F Game Score', 'Bottom 6 F Game Score',
-                                      'Top 3 D Game Score', 'Bottom 3 D Game Score',
-                                      'Starting Goalie GSAx',
-                                      'ES GF', 'ES GA', 'PP GF', 'PK GA'])
-
+# Initialize scoring data dataframe
+scoring_data = pd.DataFrame(columns=[
+    'Season', 
+    'Team', 
+    'Result',
+    '1-3 F Avg Game Scores', 
+    '4-6 F Avg Game Score', 
+    '7-9 F Avg Game Score', 
+    '9-12 F Avg Game Score',
+    '1-2 D Avg Game Score', 
+    '3-4 D Avg Game Score', 
+    '5-6 D Avg Game Score', 
+    'Starting Goalie GSAx/GP'
+])
 
 # Loop to iterate through the seasons and collect the relevant data for each team
 for season in constants.SEASONS:
@@ -155,10 +130,10 @@ for season in constants.SEASONS:
         if team_abbrev == 'UTA' and season not in(constants.UTA_SEASONS):
             team_abbrev = 'ARI'
 
-        data_row = get_relevant_data(season, team_abbrev)
+        data_row = get_scoring_data(season, team_abbrev)
 
-        relevant_data = pd.concat([relevant_data, data_row.to_frame().T], ignore_index=True)
+        scoring_data = pd.concat([scoring_data, data_row.to_frame().T], ignore_index=True)
 
 # Save relevant data as a CSV file
-save_path = f'data_relevant/all_relevant_data.csv'
-relevant_data.to_csv(save_path, index=False)
+save_path = f'relevant_data/scoring_data.csv'
+scoring_data.to_csv(save_path, index=False)
