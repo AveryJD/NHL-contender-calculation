cbc7eb93c459db3152de2b2991b91fd2479fa87f
AveryJD <averyjdoiron@icloud.com>
2025-06-25 23:53:19 -0300

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import constants
 
-
 # ====================================================================================================
 # SCRIPT TO FIX INCONSISTENT TEAM ABBREVIATIONS IN RAW DATA FILES
 # ====================================================================================================
@@ -12,10 +11,10 @@ for season in constants.SEASONS:
     for data_set in ['skaters', 'goalies', 'teams']:
 
         # Load the CSV file
-        file_path = f'data_raw/{season}_{data_set}.csv'
+        file_path = f'raw_data/team_data/{season}_{data_set}.csv'
         df = pd.read_csv(file_path)
 
-        # Define replacements
+        # Define replacements for inconsistent abreviations
         replacements = {
             'L.A': 'LAK',
             'N.J': 'NJD',
@@ -26,7 +25,7 @@ for season in constants.SEASONS:
         # Replace values across the entire DataFrame
         df = df.replace(replacements, regex=False)
 
-        # Save data
+        # Save updated CSV file
         df.to_csv(file_path, index=False)
 
 
@@ -35,18 +34,19 @@ for season in constants.SEASONS:
 # SCRIPT TO CHANGE TEAM NAMES TO THEIR ABREVIATIONS IN STANDINGS FILES
 # ====================================================================================================
 
-
 for season in constants.SEASONS:
-    file_path = f'data_standings/{season}.csv'
-    
-    # Read CSV and rename the 2nd column to 'Team'
+
+    # Load the CSV file
+    file_path = f'raw_data/standings_data/{season}.csv'
     df = pd.read_csv(file_path, header=0)
+
+    # Rename the 2nd column to 'Team'
     if df.columns[1] != 'Team':
         df.columns.values[1] = 'Team'
 
     # Replace full names with abbreviations
     df['Team'] = df['Team'].map(constants.TEAM_NAME_MAP).fillna(df['Team'])  
 
-    # Save updated CSV
+    # Save updated CSV file
     df.to_csv(file_path, index=False)
 
